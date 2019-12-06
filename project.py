23d44688393b66a25e6292b8599789ebbedd3984
alonzno <alonzoa@cs.uoregon.edu>
2019-12-05 22:01:37 -0800

    Begin transition to flask templates

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 #Flask imports
-from flask import Flask
+from flask import Flask, render_template
 
 #Database imports
 from sqlalchemy import create_engine
@@ -17,22 +17,24 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
-def HelloWorld(restaurant_id):
+def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    
-    output = ''
 
-    for i in items:
-        output += i.name
-        output += '<br>'
-        output += i.price
-        output += '<br>'
-        output += i.description
-        output += '<br>'
-        output += '<br>'
-    
-    return output
+    html = render_template('menu.html', restaurant=restaurant, items = items)
+    return html
+
+@app.route('/restaurants/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item."
+
+@app.route('/restaurants/<int:restaurant_id>/edit/<int:menu_id>/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a new menu item."
+
+@app.route('/restaurants/<int:restaurant_id>/delete/<int:menu_id>/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a new menu item."
 
 if __name__ == "__main__":
     app.debug = True
